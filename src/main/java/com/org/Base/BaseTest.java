2e0fa0d74047b2ccb23d8c6da490a43f905b8da3
Arunika <110450170+arunikaa@users.noreply.github.com>
2024-10-10 15:47:34 +0530

    Add HTML Report and screenshots
    Source code is updated with 'Adding HTML' test results and screenshots for error

--- src/main/java/com/org/Base/BaseTest.java
+++ src/main/java/com/org/Base/BaseTest.java
@@ -1,23 +1,33 @@
 package com.org.Base;// BaseTest.java
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
-
-
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
+
 
 public class BaseTest {
 
     protected static WebDriver driver;
     protected static Logger log;
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
 
     private final By acceptCookies = By.xpath("//button[@id='onetrust-accept-btn-handler']");
 
@@ -34,19 +44,60 @@ public class BaseTest {
         driver.findElement(acceptCookies).click();
         log = LogManager.getLogger(BaseTest.class);
         log.info("Driver set up is successful");
+
+        // Set up ExtentReports
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("ExtentReports.html");
+        sparkReporter.config().setTheme(Theme.DARK);
+        sparkReporter.config().setDocumentTitle("Automation Test Report");
+        sparkReporter.config().setReportName("Test Report");
+
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+        extent.setSystemInfo("Tester", "Your Name");
     }
+
 
     @BeforeClass
     public void logClassName() {
         String className = getClass().getSimpleName();
         log.info("Start Class", className);
+        test = extent.createTest(className);
     }
 
+
+    @AfterMethod
+    public void getResult(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.fail(result.getThrowable());
+            try {
+                String screenshotPath = getScreenshot(driver, result.getName());
+                test.addScreenCaptureFromPath(screenshotPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+                log.info(e.getMessage());
+            }
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.pass("Test Passed");
+        } else {
+            test.skip("Test Skipped");
+        }
+    }
+
+    public String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String destination = System.getProperty("user.dir") + "/Reports/Screenshots/" + screenshotName + dateName + ".png";
+        File finalDestination = new File(destination);
+        FileHandler.copy(source, finalDestination);
+        return destination;
+    }
 
    @AfterSuite
     public void tearDown() {
       driver.quit();
         log.info("Driver quit");
+       extent.flush();
   }
 }
 
